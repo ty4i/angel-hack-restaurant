cd2c40266f9cdfaf887b509e015fe92848bb1460
taishi <taishi@taishi.local>
2018-06-17 04:05:40 +0900

    [Fix] minor

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require 'dotenv'
 require 'sinatra'
 require 'line/bot'
 require 'pg'
-require './src/get_db_connection'
+#require './src/get_db_connection'
 require './src/line'
 require 'dotenv'
 Dotenv.load
