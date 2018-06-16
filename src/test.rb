a52c5ed945ed5b1d32e8fe65b31839e99051a176
Tirol_JPN <Tirol.JPN@gmail.com>
2018-06-17 01:10:47 +0900

    pg追加、　db追加処理途中

---
+++ src/test.rb
@@ -0,0 +1,42 @@
+# app.rb
+require 'dotenv'
+require 'sinatra'
+require 'line/bot'
+
+def client
+  @client ||= Line::Bot::Client.new { |config|
+    Dotenv.load ".env"
+    config.channel_secret = ENV['CHANNEL_SECRET']
+    config.channel_token = ENV['CHANNEL_ACCESS_TOKEN']
+  }
+end
+
+post '/callback' do
+  body = request.body.read
+
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+  unless client.validate_signature(body, signature)
+    error 400 do 'Bad Request' end
+  end
+
+  events = client.parse_events_from(body)
+  events.each { |event|
+    case event
+    when Line::Bot::Event::Message
+      case event.type
+      when Line::Bot::Event::MessageType::Text
+
+        message = {
+          type: 'text',
+          text: (p event) #event.message['text']
+        }
+        client.reply_message(event['replyToken'], message)
+      # when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+      #   response = client.get_message_content(event.message['id'])
+      #   tf = Tempfile.open("content")
+      #   tf.write(response.body)
+      # end
+    end
+  }
+  "OK"
+end
