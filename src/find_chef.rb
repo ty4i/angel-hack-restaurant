decb00d35572e1e96ab01f4a930121411b7c5c82
TirolJPN <tirol.jpn@gmail.com>
2018-06-17 11:37:59 +0900

    Update find_chef.rb

--- src/find_chef.rb
+++ src/find_chef.rb
@@ -6,7 +6,7 @@ def find_chef
       "type": "image_carousel",
       "columns": [
         {
-          "imageUrl": "https://c-www.gnst.jp/dressing/image/article/72/20972/carousel_c9d2d09ad60c62f0c1b5f7e6558e5997.jpg",
+          "imageUrl": "http://asset.recipe-blog.jp/cache/images/item/1b/c5/9707101516eb3c92f668efcf643c158252451bc5.400x0.none.jpg
           "action": {
             "type": "message",
             "label": "Matching",
@@ -14,7 +14,7 @@ def find_chef
           }
         },
         {
-          "imageUrl": "https://www.napolipizza.jp/wp/wp-content/uploads/2016/03/10-07.jpg",
+          "imageUrl": "https://img.cpcdn.com/recipes/2615799/280/367cec57efcbd804dd73581b98de6fd6.jpg?u=921598&p=1399521415",
           "action": {
             "type": "message",
             "label": "Matching",
@@ -22,7 +22,7 @@ def find_chef
           }
         },
         {
-          "imageUrl": "https://cdn.macaro-ni.jp/assets/img/shutterstock/shutterstock_236342974.jpg",
+          "imageUrl": "https://img.cpcdn.com/recipes/916497/280/00f02fc57557f391ac77325baa9f7fd7.jpg?u=1061030&p=1450157226",
           "action": {
             "type": "message",
             "label": "Matching",
@@ -30,15 +30,7 @@ def find_chef
           }
         },
         {
-          "imageUrl": "https://www.jefb.co.jp/files/menus/00000117_eye_catch.png",
-          "action": {
-            "type": "message",
-            "label": "Matching",
-            "text": "matching"
-          }
-        },
-        {
-          "imageUrl": "https://vignette.wikia.nocookie.net/computervision/images/3/34/Lenna.jpg",
+          "imageUrl": "https://cdn.oceans-nadia.com/upload/save_image_624_770/ae/ae20ee1c6cf4.JPG",
           "action": {
             "type": "message",
             "label": "Matching",
